875cd6f1b4b0d8dbf54ec8e76478b5c41a74c110
Tomek Szymczyszyn <tomasz.szymczyszyn@ironin.pl>
2015-02-21 17:02:09 +0100

    Enable hstore extension

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212121636) do
+ActiveRecord::Schema.define(version: 20150221153704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "clone_libraries", primary_key: "library_name", force: :cascade do |t|
     t.text "library_type",                default: "unspecified", null: false
