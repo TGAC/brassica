ca114f77d883827071292ded0928c57700729e76
Tomek Szymczyszyn <tomasz.szymczyszyn@ironin.pl>
2015-02-26 20:45:23 +0100

    Replace sketchy query and placeholder data with AR queries

--- app/forms/concerns/model_helper.rb
+++ app/forms/concerns/model_helper.rb
@@ -1,17 +1,15 @@
 module ModelHelper
   def population_types
-    ["Type A", "Type B", "Type C"]
+    PopulationTypeLookup.order(:population_type).pluck(:population_type)
   end
 
   def plant_lines(name = nil)
-    # FIXME replace with proper AR query
-    query = "SELECT plant_line_name FROM plant_lines "
-    query += "WHERE plant_line_name ILIKE '%#{name}%'" if name
-    results = ActiveRecord::Base.connection.select_all(query)
-    results.map { |r| r['plant_line_name'] }
+    results = PlantLine.order(:plant_line_name)
+    results = results.where('plant_line_name ILIKE ?', "%#{name}%")
+    results.pluck(:plant_line_name)
   end
 
   def taxonomy_terms
-    TaxonomyTerm.pluck(:name).unshift(nil)
+    TaxonomyTerm.pluck(:name)
   end
 end
