449b630a63d6f88800601884985773eddfb5301c
gubala <tg.errors@gmail.com>
2015-02-26 15:18:24 +0100

    scope to reflect the CS populations data grid

--- app/models/plant_population.rb
+++ app/models/plant_population.rb
@@ -14,4 +14,37 @@ class PlantPopulation < ActiveRecord::Base
                           join_table: 'plant_population_lists',
                           association_foreign_key: 'plant_line_name'
 
+  # Original CropStore query
+  # plant_populations.species as Species,
+  # plant_populations.canonical_population_name as Population,
+  # plant_populations.female_parent_line as 'Female Parent',
+  # plant_populations.male_parent_line as 'Male Parent',
+  # plant_populations.population_type as Type,
+  # count( plant_population_id ) as 'Number Of Subpopulations' ,
+  # count( linkage_maps.mapping_population ) as 'Number Of Maps'
+  # FROM plant_populations
+  # LEFT JOIN pop_type_lookup ON pop_type_lookup.population_type=plant_populations.population_type
+  # LEFT JOIN linkage_maps ON linkage_maps.mapping_population = plant_populations.plant_population_id
+  # GROUP BY plant_populations.canonical_population_name
+  # ORDER BY canonical_population_name
+  scope :grid_data, -> do
+    columns = [
+      :species,
+      :canonical_population_name,
+      :female_parent_line,
+      :male_parent_line,
+      :population_type
+    ]
+
+    select(columns).
+      drop_dummies.
+      group(columns).
+      order(:canonical_population_name).
+      count(:plant_population_id)
+  end
+
+  # Exlude the ['none', 'unspecified', 'not applicable'] pseudo-record trio
+  scope :drop_dummies, -> do
+    where.not(canonical_population_name: '')
+  end
 end
