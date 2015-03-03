b1e8a64b1f615bae3f2243b491a33aa5560adce9
Tomek Szymczyszyn <tomasz.szymczyszyn@ironin.pl>
2015-03-03 21:25:33 +0100

    Added FIXME for PlantLinesController#index

--- app/controllers/plant_lines_controller.rb
+++ app/controllers/plant_lines_controller.rb
@@ -1,6 +1,7 @@
 class PlantLinesController < ApplicationController
   include ModelHelper
 
+  # FIXME temporary, to be merged with plant_populations_data_grid branch
   def index
     if params[:name]
       render json: plant_lines(params[:name]).map { |r| { id: r, text: r } }
