7bbe290bd0f61d30c99148ae4fa673d12b51695d
gubala <tg.errors@gmail.com>
2015-03-13 19:06:48 +0100

    removed surplus decorator structure, fixed controller/decorator specs

--- app/models/plant_population.rb
+++ app/models/plant_population.rb
@@ -22,7 +22,7 @@ class PlantPopulation < ActiveRecord::Base
 
   def self.grid_data
     columns = [
-      :plant_population_id,
+      'plant_populations.plant_population_id',
       'plant_populations.species',
       :canonical_population_name,
       :female_parent_line,
@@ -31,39 +31,10 @@ class PlantPopulation < ActiveRecord::Base
     ]
 
     select(columns + ['count(plant_lines.plant_line_name)']).
-      joins(:plant_lines).
+      includes(:plant_lines).
       group(columns).
       drop_dummies.
       order(:plant_population_id).
       pluck(*(columns + ['count(plant_lines.plant_line_name)']))
   end
-
-  # # Original CropStore query
-  # # plant_populations.species as Species,
-  # # plant_populations.canonical_population_name as Population,
-  # # plant_populations.female_parent_line as 'Female Parent',
-  # # plant_populations.male_parent_line as 'Male Parent',
-  # # plant_populations.population_type as Type,
-  # # count( plant_population_id ) as 'Number Of Subpopulations' ,
-  # # count( linkage_maps.mapping_population ) as 'Number Of Maps'
-  # # FROM plant_populations
-  # # LEFT JOIN pop_type_lookup ON pop_type_lookup.population_type=plant_populations.population_type
-  # # LEFT JOIN linkage_maps ON linkage_maps.mapping_population = plant_populations.plant_population_id
-  # # GROUP BY plant_populations.canonical_population_name
-  # # ORDER BY canonical_population_name
-  # def self.grid_data
-  #   columns = [
-  #     :species,
-  #     :canonical_population_name,
-  #     :female_parent_line,
-  #     :male_parent_line,
-  #     :population_type
-  #   ]
-  #
-  #   select(columns).
-  #   drop_dummies.
-  #   group(columns).
-  #   order(:canonical_population_name).
-  #   count(:plant_population_id)
-  # end
 end
