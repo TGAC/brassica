78ac5bccf10c7795a08e77bebdb61e954b536e62
gubala <tg.errors@gmail.com>
2015-03-15 11:19:09 +0100

    simple sql script for easier dev db setup

---
+++ db/prepare_db.sql
@@ -0,0 +1,3 @@
+CREATE TABLE schema_migrations (version VARCHAR(40) NOT NULL);
+INSERT INTO schema_migrations(version) VALUES('0');
+INSERT INTO schema_migrations(version) VALUES ('20150212100000');
