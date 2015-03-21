099139911f2e78a2ac8229f75f351cc795cd6cd8
Piotr Nowakowski <ymnowako@cyf-kr.edu.pl>
2015-03-21 22:00:10 +0100

    Create missing plant_part records and fix 1:n relationship between plant_parts and plant_scoring_units

--- app/models/plant_part.rb
+++ app/models/plant_part.rb
@@ -1,3 +1,5 @@
 class PlantPart < ActiveRecord::Base
-  
+
+  has_many :plant_scoring_units, foreign_key: 'scored_plant_part'
+
 end
--- app/models/plant_scoring_unit.rb
+++ app/models/plant_scoring_unit.rb
@@ -3,6 +3,7 @@ class PlantScoringUnit < ActiveRecord::Base
   belongs_to :design_factor
   belongs_to :plant_trial
   belongs_to :plant_accession, foreign_key: 'plant_accession'
+  belongs_to :plant_part, foreign_key: 'scored_plant_part'
 
   has_many :trait_scores, foreign_key: 'scoring_unit_id'
 
