a34abe63dd00a8199031856fdcffeff423188aaf
gubala <tg.errors@gmail.com>
2015-03-27 08:54:07 +0100

    introduced simple navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,37 @@
 module ApplicationHelper
+  def active_nav_label
+    case controller_name
+      when 'application'
+        case action_name
+          when 'index'
+            'Home'
+          when 'about'
+            'About us'
+          else
+            'Home'
+        end
+      when 'plant_populations', 'plant_lines'
+        'Browse database'
+      when 'submissions'
+        'Submit data'
+      else
+        'Home'
+    end
+  end
+
+  def active_link(label)
+    content_tag :li, class: 'active' do
+      link_to label, '#'
+    end
+  end
+
+  def navbar_menu
+    [
+      ['Home', root_path],
+      ['Browse database', plant_populations_path],
+      ['Submit data', new_submission_path],
+      ['API documentation', '#'],
+      ['About us', about_path]
+    ]
+  end
 end
