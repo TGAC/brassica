19df67ab2fc7a73fe24b7b1bda8854cdeaedbf0a
Piotr Nowakowski <ymnowako@cyf-kr.edu.pl>
2015-04-11 18:03:28 +0200

    Model relation names converted to lowercase.

--- app/models/marker_assay.rb
+++ app/models/marker_assay.rb
@@ -2,14 +2,14 @@ class MarkerAssay < ActiveRecord::Base
 
   belongs_to :marker_sequence_assignment
 
-  belongs_to :restriction_enzyme_A, class_name: 'RestrictionEnzyme',
+  belongs_to :restriction_enzyme_a, class_name: 'RestrictionEnzyme',
               foreign_key: 'restriction_enzyme_a_id'
-  belongs_to :restriction_enzyme_B, class_name: 'RestrictionEnzyme',
+  belongs_to :restriction_enzyme_b, class_name: 'RestrictionEnzyme',
               foreign_key: 'restriction_enzyme_b_id'
 
-  belongs_to :primer_A, class_name: 'Primer',
+  belongs_to :primer_a, class_name: 'Primer',
              foreign_key: 'primer_a_id'
-  belongs_to :primer_B, class_name: 'Primer',
+  belongs_to :primer_b, class_name: 'Primer',
              foreign_key: 'primer_b_id'
 
   belongs_to :probe
--- app/models/primer.rb
+++ app/models/primer.rb
@@ -1,12 +1,12 @@
 class Primer < ActiveRecord::Base
 
-  has_many :marker_assays_A, class_name: 'MarkerAssay',
+  has_many :marker_assays_a, class_name: 'MarkerAssay',
              foreign_key: 'primer_a_id'
-  has_many :marker_assays_B, class_name: 'MarkerAssay',
+  has_many :marker_assays_b, class_name: 'MarkerAssay',
              foreign_key: 'primer_b_id'
 
   def marker_assays
-    marker_assays_A | marker_assays_B
+    marker_assays_a | marker_assays_b
   end
 
   include Annotable
