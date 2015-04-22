bf7fa24c6b12d8b6a058ce73ae8275fc809c869d
Tomasz Gubała <t.gubala@cyfronet.pl>
2015-04-22 21:46:56 +0200

    new validation and set of model methods

--- spec/factories/submission.rb
+++ spec/factories/submission.rb
@@ -2,5 +2,12 @@ FactoryGirl.define do
   factory :submission do
     submission_type :population
     user
+
+    trait :finalized do
+      finalized true
+      submitted_object_id { create(:plant_population).id }
+    end
+
+    factory :finalized_submission, traits: [:finalized]
   end
 end
