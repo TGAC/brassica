41f097fa30c2007aecfae17e8f60c0194d57bfe5
Tomasz Gubała <t.gubala@cyfronet.pl>
2015-04-24 12:57:32 +0200

    views updated to handle new related counter column

--- spec/models/model_spec.rb
+++ spec/models/model_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ActiveRecord::Base do
-  it 'defines table and ref columns as strings only' do
+  before(:all) do
     Rails.application.eager_load!
+  end
+
+  it 'defines table and ref columns as strings only' do
     ActiveRecord::Base.descendants.each do |model|
       if model.respond_to? 'table_columns'
         expect(model.table_columns).to all be_an(String)
@@ -10,6 +13,14 @@ RSpec.describe ActiveRecord::Base do
       if model.respond_to? 'ref_columns'
         expect(model.ref_columns).to all be_an(String)
       end
+    end
+  end
+
+  it 'responds with nonempty table columns when permitted for data tables' do
+    allowed_models = DataTablesController.new.send(:allowed_models)
+    allowed_models.each do |model|
+      expect(model.classify.constantize).to respond_to(:table_columns)
+      expect(model.classify.constantize.table_columns).not_to be_empty
     end
   end
 end
--- spec/views/data_tables/index.html.haml_spec.rb
+++ spec/views/data_tables/index.html.haml_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe 'data_tables/index.html.haml' do
     it 'contains additional count columns' do
       render
       expect(rendered).to include t('tables.qtl.id')
-      expect(rendered).to include t('tables.trait_descriptors.trait_scores_count')
+      expect(rendered).to include t('tables.trait_descriptors.trait_scores')
     end
   end
 
@@ -45,7 +45,7 @@ RSpec.describe 'data_tables/index.html.haml' do
 
   context 'when run for annotable models' do
     it 'shows annotations column' do
-      annotable_tables.each do |table|
+      (displayable_tables & annotable_tables).each do |table|
         allow(view).to receive(:params).and_return(model: table)
         render
         expect(rendered).to include(table)
@@ -67,13 +67,10 @@ RSpec.describe 'data_tables/index.html.haml' do
   end
 
   it 'has all table and count column names translated' do
-    annotable_tables.each do |table|
+    displayable_tables.each do |table|
       allow(view).to receive(:params).and_return(model: table)
       model_klass = table.singularize.camelize.constantize
-      columns = []
-      columns += model_klass.table_columns if model_klass.respond_to? :table_columns
-      columns += model_klass.count_columns if model_klass.respond_to? :count_columns
-      columns.each do |column|
+      model_klass.table_columns.each do |column|
         _table, _column = view.extract_column(column)
         expect(I18n.t("tables.#{_table}.#{_column}")).
           not_to include 'translation missing'
