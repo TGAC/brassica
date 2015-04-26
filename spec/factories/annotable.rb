e9084051ffd9da679ca831a94d82f8f41448b62c
Tomasz Szymczyszyn <tomasz.szymczyszyn@gmail.com>
2015-04-26 21:58:23 +0200

    Fix some factories which did not randomize model attributes

--- spec/factories/annotable.rb
+++ spec/factories/annotable.rb
@@ -1,16 +1,16 @@
 FactoryGirl.define do
   trait :annotable do
-    comments Faker::Lorem.sentence
-    entered_by_whom Faker::Internet.user_name
-    date_entered Faker::Date.backward
-    data_provenance Faker::Lorem.sentence
-    data_owned_by Faker::Company.name
+    comments { Faker::Lorem.sentence }
+    entered_by_whom { Faker::Internet.user_name }
+    date_entered { Faker::Date.backward }
+    data_provenance { Faker::Lorem.sentence }
+    data_owned_by { Faker::Company.name }
   end
 
   trait :annotable_no_owner do
-    comments Faker::Lorem.sentence
-    entered_by_whom Faker::Internet.user_name
-    date_entered Faker::Date.backward
-    data_provenance Faker::Lorem.sentence
+    comments { Faker::Lorem.sentence }
+    entered_by_whom { Faker::Internet.user_name }
+    date_entered { Faker::Date.backward }
+    data_provenance { Faker::Lorem.sentence }
   end
 end
--- spec/factories/plant_line.rb
+++ spec/factories/plant_line.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :plant_line do
     sequence(:plant_line_name) { |n| "#{Faker::Lorem.characters(5)}_#{n}" }
-    common_name Faker::Lorem.word
-    previous_line_name Faker::Lorem.word
-    organisation Faker::Company.name
-    genetic_status Faker::Lorem.word
+    common_name { Faker::Lorem.word }
+    previous_line_name { Faker::Lorem.word }
+    organisation { Faker::Company.name }
+    genetic_status { Faker::Lorem.word }
     taxonomy_term
     annotable
   end
