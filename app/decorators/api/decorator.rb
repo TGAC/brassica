cf59405134c9a99079dde926b4dc49f4903a63c9
Tomasz Szymczyszyn <tomasz.szymczyszyn@gmail.com>
2015-05-02 21:48:04 +0200

    Return all object attributes in the API responses

--- app/decorators/api/decorator.rb
+++ app/decorators/api/decorator.rb
@@ -1,12 +1,7 @@
 class Api::Decorator < Draper::Decorator
   delegate_all
 
-  # FIXME check if these are ok
-  def api_attribute_names
-    object.class.table_columns + object.class.ref_columns
-  end
-
   def as_json(*)
-    object.as_json(only: api_attribute_names)
+    object.as_json
   end
 end
