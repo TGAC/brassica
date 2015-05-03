2bf62616eca39bf4efb09e048db4fe7ded646683
Tomasz Szymczyszyn <tomasz.szymczyszyn@gmail.com>
2015-05-03 16:49:52 +0200

    Each new user gets API key generated automatically

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,13 @@ class User < ActiveRecord::Base
 
   validates :login, presence: true, uniqueness: { case_sensitive: false }
 
+  after_create :create_api_key!
+
   def self.find_or_create_from_auth_hash(auth_hash)
     user = self.find_or_initialize_by(login: auth_hash['uid'])
     user.full_name = auth_hash['full_name'] if auth_hash['full_name']
     user.save
     user
   end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,9 @@ RSpec.describe User, type: :model do
     expect{ u.destroy }.to raise_error 'Entities of User cannot be destroyed!'
     expect(User.count).to eq 1
   end
+
+  it 'gets api key on creation' do
+    user = create(:user, api_key: nil)
+    expect(user.reload.api_key).to be_present
+  end
 end
