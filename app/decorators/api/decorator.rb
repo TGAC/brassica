e12d5264a50374170e22f329b39d09ebb80ffa2a
Tomasz Szymczyszyn <tomasz.szymczyszyn@gmail.com>
2015-05-03 17:12:32 +0200

    Add NOTE for Api::Decorator

--- app/decorators/api/decorator.rb
+++ app/decorators/api/decorator.rb
@@ -1,7 +1,5 @@
+# NOTE it does not do anything at this moment but will be used to expose
+# associations
 class Api::Decorator < Draper::Decorator
   delegate_all
-
-  def as_json(*)
-    object.as_json
-  end
 end
