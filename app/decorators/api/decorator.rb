d156030f930f13b06e1da21a8dde83159e99b994
Tomasz Szymczyszyn <tomasz.szymczyszyn@gmail.com>
2015-05-08 17:14:00 +0200

    Expose has_many associations in API responses

--- app/decorators/api/decorator.rb
+++ app/decorators/api/decorator.rb
@@ -2,4 +2,32 @@
 # associations
 class Api::Decorator < Draper::Decorator
   delegate_all
+
+  def as_json(*)
+    super.merge(associations_as_json)
+  end
+
+  def associations_as_json
+    {}.tap do |json|
+      object.class.reflections.each do |association, reflection|
+        next if reflection.is_a?(ActiveRecord::Reflection::BelongsToReflection) # These are already included
+        next if blacklisted_has_many_association?(association)
+
+        association_klass = (reflection.options[:class_name] || association.classify).constantize
+        primary_key = reflection.options[:primary_key] || association_klass.primary_key
+
+        next unless primary_key # Skip associations with join tables used by HABTM
+
+        attr = "#{association}_#{primary_key.to_s.pluralize}"
+
+        json[attr] = object.send(association).pluck(primary_key)
+      end
+    end
+  end
+
+  private
+
+  def blacklisted_has_many_association?(name)
+    %w(User Submission ApiKey).include?(name.classify)
+  end
 end
---
+++ spec/decorators/api/decorator_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+RSpec.describe Api::Decorator do
+
+  subject { described_class.new(object) }
+
+  let(:json) { subject.as_json }
+
+  describe "#as_json" do
+    context "decorating PlantLine" do
+      let!(:object) { create :plant_line, :with_has_many_associations }
+
+      it "includes has_many associations" do
+        expect(json['fathered_descendants_ids']).to eq object.fathered_descendants.pluck(:id)
+        expect(json['mothered_descendants_ids']).to eq object.mothered_descendants.pluck(:id)
+        expect(json['plant_accessions_ids']).to eq object.plant_accessions.pluck(:id)
+        expect(json['plant_populations_ids']).to eq object.plant_populations.pluck(:id)
+      end
+    end
+
+    context "decorating PlantVariety" do
+      let!(:object) { create :plant_variety, :with_has_many_associations }
+
+      it "includes has_many associations" do
+        expect(json['plant_lines_ids']).to eq object.plant_lines.pluck(:id)
+        expect(json['countries_of_origin_ids']).to eq object.countries_of_origin.pluck(:id)
+        expect(json['countries_registered_ids']).to eq object.countries_registered.pluck(:id)
+      end
+    end
+
+    context "decorating PlantPopulation" do
+      let!(:object) { create :plant_population, :with_has_many_associations }
+
+      it "includes has_many associations" do
+        expect(json['plant_lines_ids']).to eq object.plant_lines.pluck(:id)
+        expect(json['plant_trials_ids']).to eq object.plant_trials.pluck(:id)
+        expect(json['population_loci_ids']).to eq object.population_loci.pluck(:id)
+        expect(json['linkage_maps_ids']).to eq object.linkage_maps.pluck(:id)
+      end
+    end
+  end
+end
