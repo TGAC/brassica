4bcdc60d78a947968149cb51f9146cb577426e1a
Tomasz Szymczyszyn <tomasz.szymczyszyn@gmail.com>
2015-05-13 16:28:21 +0200

    Extract Api::AssociationFinder from Api::Decorator

---
+++ app/services/api/association_finder.rb
@@ -0,0 +1,34 @@
+class Api::AssociationFinder
+
+  attr_accessor :klass
+
+  def initialize(klass)
+    self.klass = klass
+  end
+
+  def has_many_associations
+    klass.reflections.map do |association, reflection|
+      next if reflection.is_a?(ActiveRecord::Reflection::BelongsToReflection)
+      next if blacklisted_has_many_association?(association)
+
+      association_klass = (reflection.options[:class_name] || association.classify).constantize
+      primary_key = reflection.options[:primary_key] || association_klass.primary_key
+
+      next unless primary_key # Skip associations with join tables used by HABTM
+
+      OpenStruct.new(
+        name: association,
+        primary_key: primary_key,
+        param: "#{association}_#{primary_key.to_s.pluralize}",
+        class_name: association_klass.name
+
+      )
+    end.compact
+  end
+
+  private
+
+  def blacklisted_has_many_association?(name)
+    %w(User Submission ApiKey).include?(name.classify)
+  end
+end
