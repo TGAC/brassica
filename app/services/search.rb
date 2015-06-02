9eecc9bb7a64b5ab6860da37c07cac2ae6b7a756
Tomasz Szymczyszyn <tomasz.szymczyszyn@gmail.com>
2015-06-02 17:55:25 +0200

    DRYing up Search class

--- app/services/search.rb
+++ app/services/search.rb
@@ -2,6 +2,16 @@ class Search
 
   attr_accessor :query, :wildcarded_query
 
+  def self.searchable_klasses
+    @searchable_klasses ||= ActiveRecord::Base.descendants.select { |klass|
+      klass.ancestors.include?(Searchable)
+    }
+  end
+
+  def self.searchable_tables
+    @searchable_tables ||= searchable_klasses.map(&:table_name)
+  end
+
   def initialize(query)
     query = escape_query_special_chars(query.dup)
 
@@ -9,141 +19,69 @@ class Search
     self.wildcarded_query = add_query_wildcards(query)
   end
 
-  def counts
-    {
-      plant_populations: plant_populations.count,
-      plant_lines: plant_lines.count,
-      plant_varieties: plant_varieties.count,
-      map_locus_hits: map_locus_hits.count,
-      map_positions: map_positions.count,
-      population_loci: population_loci.count,
-      linkage_maps: linkage_maps.count,
-      linkage_groups: linkage_groups.count,
-      marker_assays: marker_assays.count,
-      primers: primers.count,
-      probes: probes.count,
-      plant_trials: plant_trials.count,
-      qtl: qtl.count,
-      trait_descriptors: trait_descriptors.count
-    }
-  end
-
   def all
-    {
-      plant_populations: plant_populations,
-      plant_lines: plant_lines,
-      plant_varieties: plant_varieties,
-      map_locus_hits: map_locus_hits,
-      map_positions: map_positions,
-      population_loci: population_loci,
-      linkage_maps: linkage_maps,
-      linkage_groups: linkage_groups,
-      marker_assays: marker_assays,
-      primers: primers,
-      probes: probes,
-      plant_trials: plant_trials,
-      qtl: qtl,
-      trait_descriptors: trait_descriptors
-    }
-  end
-
-  def plant_populations
-    PlantPopulation.search(wildcarded_query, size: PlantPopulation.count)
-  end
-
-  def plant_lines
-    PlantLine.search(wildcarded_query, size: PlantLine.count)
-  end
-
-  def plant_varieties
-    PlantVariety.search(wildcarded_query, size: PlantVariety.count)
-  end
-
-  def map_locus_hits
-    if numeric_query?
-      MapLocusHit.search(
-        filter: {
-          or: MapLocusHit.numeric_columns.map { |column|
-            if column.include?(".")
-              parts = column.split(".")
-              *tables, column = parts
-              tables = tables.map(&:singularize)
-              column = (tables | [column]).join(".")
-            end
-
-            { term: { column => query } }
-          }
-        },
-        size: MapLocusHit.count
-      )
-    else
-      MapLocusHit.search(wildcarded_query, size: MapLocusHit.count)
+    {}.tap do |all|
+      self.class.searchable_tables.each do |table|
+        all[table.to_sym] = results(table)
+      end
     end
   end
 
-  def map_positions
-    if numeric_query?
-      MapPosition.search(
-        filter: {
-          or: MapPosition.numeric_columns.map { |column|
-            { term: { column => query } }
-          }
-        },
-        size: MapPosition.count
-      )
-    else
-      MapPosition.search(wildcarded_query, size: MapPosition.count)
+  def counts
+    {}.tap do |counts|
+      self.class.searchable_tables.each do |table|
+        counts[table.to_sym] = results(table).count
+      end
     end
   end
 
-  def population_loci
-    PopulationLocus.search(wildcarded_query, size: PopulationLocus.count)
-  end
-
-  def linkage_groups
-    LinkageGroup.search(wildcarded_query, size: LinkageGroup.count)
-  end
-
-  def linkage_maps
-    LinkageMap.search(wildcarded_query, size: LinkageMap.count)
-  end
-
-  def marker_assays
-    MarkerAssay.search(wildcarded_query, size: MarkerAssay.count)
-  end
-
-  def primers
-    Primer.search(wildcarded_query, size: Primer.count)
-  end
-
-  def probes
-    Probe.search(wildcarded_query, size: Probe.count)
-  end
-
-  def plant_trials
-    PlantTrial.search(wildcarded_query, size: PlantTrial.count)
-  end
-
-  def qtl
-    if numeric_query?
-      Qtl.search(
-        filter: {
-          or: Qtl.numeric_columns.map { |column|
-            { term: { column => query } }
-          }
-        },
-        size: Qtl.count
-      )
+  def results(table)
+    if numeric_query? && table.classify.constantize.numeric_columns.present?
+      filter(table)
     else
-      Qtl.search(wildcarded_query, size: Qtl.count)
+      search(table)
     end
   end
 
-  def trait_descriptors
-    TraitDescriptor.search(wildcarded_query, size: TraitDescriptor.count)
+  def search(table)
+    klass = table.classify.constantize
+    klass.search(wildcarded_query, size: klass.count)
+  end
+
+  def filter(table)
+    klass = table.classify.constantize
+
+    klass.search(
+      filter: {
+        or: klass.numeric_columns.map { |column|
+          if column.include?(".")
+            parts = column.split(".")
+            *tables, column = parts
+            tables = tables.map(&:singularize)
+            column = (tables | [column]).join(".")
+          end
+
+          { term: { column => query } }
+        }
+      },
+      size: klass.count
+    )
   end
 
   private
+
+  def method_missing(name, *args, &block)
+    if self.class.searchable_tables.include?(name.to_s)
+      raise ArgumentError, "No args expected" if args.present?
+      results(name.to_s)
+    else
+      super
+    end
+  end
+
+  def respond_to_missing?(name, include_private = false)
+    self.class.searchable_tables.include?(name.to_s) or super
+  end
 
   def escape_query_special_chars(query)
     special_chars.each { |chr| query.gsub!(chr, "\\#{chr}") }
@@ -161,4 +99,5 @@ class Search
   def numeric_query?
     query =~ /\A-?\d+(\.\d+)?\z/
   end
+
 end
