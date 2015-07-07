079c562bfdb8360cf822b832550d1924eee16d4a
Tomasz Szymczyszyn <tomasz.szymczyszyn@gmail.com>
2015-07-07 15:07:26 +0200

    Fix spec for Submissions::UploadsController

--- spec/factories/upload.rb
+++ spec/factories/upload.rb
@@ -3,7 +3,7 @@ include ActionDispatch::TestProcess
 FactoryGirl.define do
   factory :upload, class: Submission::Upload do
     upload_type 'trait_scores'
-    file { fixture_file_upload(Rails.root.join('spec', 'support', 'score_upload.txt'), 'text/plain') }
+    file { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'score_upload.txt'), 'text/plain') }
     association :submission, submission_type: :trial
   end
 end
--- spec/requests/submissions/uploads_spec.rb
+++ spec/requests/submissions/uploads_spec.rb
@@ -16,16 +16,26 @@ RSpec.describe "Submission uploads" do
 
   context "with user signed in" do
     let(:user) { submission.user }
+    let(:parsed_response) { JSON.parse(response.body) }
 
     before { login_as(user) }
 
     describe "POST /submissions/:submission_id/uploads" do
-      let(:file) { Tempfile.new("upload") }
+      let(:file) { fixture_file_upload('files/score_upload.txt', 'text/plain') }
 
       it "creates upload" do
-        post "/submissions/#{submission.id}/uploads", submission_upload: { file: file }
+        expect {
+          post "/submissions/#{submission.id}/uploads", submission_upload: {
+            upload_type: 'trait_scores',
+            file: file
+          }
+        }.to change { submission.uploads.count }.from(0).to(1)
 
-        binding.pry
+        expect(response).to be_success
+        expect(parsed_response).to include(
+          "file_file_name" => "score_upload.txt",
+          "delete_url" => submission_upload_path(submission, submission.uploads.last)
+        )
       end
     end
   end
