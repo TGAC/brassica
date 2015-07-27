6d0ffb8184d9e523b0f9b45fa6e07b289667394b
Tomasz Szymczyszyn <tomasz.szymczyszyn@gmail.com>
2015-07-27 11:49:23 +0200

    Enable paging for input fields with autocomplete

--- app/controllers/plant_populations_controller.rb
+++ app/controllers/plant_populations_controller.rb
@@ -1,8 +1,14 @@
-# FIXME merge all autocomplete related controllers
 class PlantPopulationsController < ApplicationController
 
   def index
-    render json: PlantPopulation.filter(params)
+    page = params[:page] || 1
+    plant_populations = PlantPopulation.filter(params).order(:name)
+    render json: {
+      results: plant_populations.page(page),
+      page: page,
+      per_page: Kaminari.config.default_per_page,
+      total_count: plant_populations.count
+    }
   end
 
 end
--- app/controllers/trait_descriptors_controller.rb
+++ app/controllers/trait_descriptors_controller.rb
@@ -1,9 +1,16 @@
 class TraitDescriptorsController < ApplicationController
 
   def index
+    page = params[:page] || 1
     descriptors = TraitDescriptor.where("descriptor_name ILIKE ?", "%#{params[:search][:descriptor_name]}%")
+    descriptors = descriptors.order(:descriptor_name)
 
-    render json: descriptors
+    render json: {
+      results: descriptors.page(page),
+      page: page,
+      per_page: Kaminari.config.default_per_page,
+      total_count: descriptors.count
+    }
   end
 
 end
