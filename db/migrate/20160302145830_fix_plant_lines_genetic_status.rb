faad3dc5c89ac360cd212ba3cf3648f84b82741b
Piotr Nowakowski <ymnowako@cyf-kr.edu.pl>
2016-03-31 12:20:21 +0200

    Technical commit to force repeat of Travis test.

--- db/migrate/20160302145830_fix_plant_lines_genetic_status.rb
+++ db/migrate/20160302145830_fix_plant_lines_genetic_status.rb
@@ -2,4 +2,5 @@ class FixPlantLinesGeneticStatus < ActiveRecord::Migration
   def up
     execute("UPDATE plant_lines SET genetic_status = 'Open Pollinated' WHERE genetic_status = 'Open Polinated'")
   end
+
 end
