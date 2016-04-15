f11fd75923e96290887d5d117ae0b3537ef1c9e9
Piotr Nowakowski <ymnowako@cyf-kr.edu.pl>
2016-04-15 14:08:16 +0200

    Add missing indices for published and user_id columns.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329190215) do
+ActiveRecord::Schema.define(version: 20160415135850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
