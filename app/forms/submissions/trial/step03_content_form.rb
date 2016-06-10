80809e489ad8b5584c76db597129fb9ce6eec79c
Tomasz Gubała <t.gubala@cyfronet.pl>
2016-06-10 21:24:23 +0200

    removed writeable from reform class properties; removed permitted parameters method that was not useful

--- app/forms/submissions/trial/step03_content_form.rb
+++ app/forms/submissions/trial/step03_content_form.rb
@@ -1,9 +1,9 @@
 module Submissions
   module Trial
     class Step03ContentForm < PlantTrialForm
-      property :lines_or_varieties, writeable: false
-      property :technical_replicate_numbers, writeable: false
-      property :design_form_names, writeable: false
+      property :lines_or_varieties
+      property :technical_replicate_numbers
+      property :design_form_names
     end
   end
 end
--- app/forms/submissions/trial/step04_content_form.rb
+++ app/forms/submissions/trial/step04_content_form.rb
@@ -1,27 +1,14 @@
 module Submissions
   module Trial
     class Step04ContentForm < PlantTrialForm
-      property :trait_mapping, writeable: false
-      property :trait_scores, writeable: false
-      property :accessions, writeable: false
-      property :lines_or_varieties, writeable: false
-      property :replicate_numbers, writeable: false
-      property :design_factor_names, writeable: false
-      property :design_factors, writeable: false
+      property :trait_mapping
+      property :trait_scores
+      property :accessions
+      property :lines_or_varieties
+      property :replicate_numbers
+      property :design_factor_names
+      property :design_factors
       property :upload_id
-
-      def self.permitted_properties
-        [
-          :trait_mapping,
-          :trait_scores,
-          :accessions,
-          :lines_or_varieties,
-          :replicate_numbers,
-          :design_factor_names,
-          :design_factors,
-          :upload_id
-        ]
-      end
 
       def upload
         upload = Submission::Upload.trait_scores.find_by(id: upload_id)
