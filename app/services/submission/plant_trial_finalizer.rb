f1c396a16bae8970430d63f11b63fafc00a0376b
Tomasz Gubała <t.gubala@cyfronet.pl>
2016-06-11 12:24:31 +0200

    refactored create_scoring

--- app/services/submission/plant_trial_finalizer.rb
+++ app/services/submission/plant_trial_finalizer.rb
@@ -33,74 +33,83 @@ class Submission::PlantTrialFinalizer
     end
   end
 
+  def find_or_create_plant_variety(plant_id)
+    PlantVariety.find_or_create_by!(
+      plant_variety_name: lines_or_varieties[plant_id]['relation_record_name']
+    ) do |new_plant_variety|
+      new_plant_variety.assign_attributes(common_data)
+    end
+  end
+
+  def find_or_create_plant_accession(plant_id)
+    PlantAccession.find_or_create_by!(
+      plant_accession: accessions[plant_id]['plant_accession'],
+      originating_organisation: accessions[plant_id]['originating_organisation']
+    ) do |new_plant_accession|
+      # If new accession, set PL or PV relation for that accession
+      accession_relation = {}
+
+      if lines_or_varieties[plant_id].blank?
+        raise 'Required plant line or plant variety data not available after parsing.'
+
+      elsif lines_or_varieties[plant_id]['relation_class_name'] == 'PlantVariety'
+        accession_relation = { plant_variety: find_or_create_plant_variety(plant_id) }
+
+      elsif lines_or_varieties[plant_id]['relation_class_name'] == 'PlantLine'
+        plant_line = PlantLine.find_by(
+            plant_line_name: lines_or_varieties[plant_id]['relation_record_name']
+        )
+        if plant_line
+          accession_relation = { plant_line: plant_line }
+        else
+          #TODO Inform the user there is no PlantLine of a given name and that it should be submitted with another submission first
+          rollback(2)
+        end
+
+      else
+        raise "Incorrect value [#{lines_or_varieties[plant_id]['relation_class_name']}] user for relation name for a new accession."
+      end
+
+      new_plant_accession.assign_attributes(common_data.merge(accession_relation))
+    end
+  end
+
+  def create_design_factor(plant_id)
+    design_factor = nil
+    factor_values = design_factors[plant_id]
+    if design_factor_names.present? && factor_values.present?
+      factor_attrs = {}
+      factor_values.each_with_index do |factor_value, i|
+        factor_attrs["design_factor_#{i+1}".to_sym] = if design_factor_names[i]
+                                                        "#{design_factor_names[i]}_#{factor_value}"
+                                                      else
+                                                        nil
+                                                      end
+      end
+      design_factor = DesignFactor.create!(
+        common_data.except(:user, :published, :published_on).merge(
+          design_unit_counter: factor_values.last
+        ).merge(
+          factor_attrs
+        )
+      )
+    end
+    design_factor
+  end
+
   def create_scoring
     trait_mapping = submission.content.step04.trait_mapping
     trait_scores = submission.content.step04.trait_scores || {}
-    accessions = submission.content.step04.accessions
     replicate_numbers = submission.content.step04.replicate_numbers || {}
-    design_factors = submission.content.step04.design_factors || {}
-    design_factor_names = submission.content.step04.design_factor_names || []
-    lines_or_varieties = submission.content.step04.lines_or_varieties || {}
 
     @new_plant_scoring_units = trait_scores.map do |plant_id, scores|
       unless accessions[plant_id] && accessions[plant_id]['plant_accession'] && accessions[plant_id]['originating_organisation']
         raise 'Misformed parsed plant accession data.'
       end
 
-      plant_accession = PlantAccession.find_or_create_by!(
-        plant_accession: accessions[plant_id]['plant_accession'],
-        originating_organisation: accessions[plant_id]['originating_organisation']
-      ) do |new_plant_accession|
-        # If new accession, set PL or PV
-        accession_relation = {}
+      plant_accession = find_or_create_plant_accession(plant_id)
 
-        if lines_or_varieties[plant_id].blank?
-          raise 'Required plant line or plant variety data not available after parsing.'
-        elsif lines_or_varieties[plant_id]['relation_class_name'] == 'PlantVariety'
-          plant_variety = PlantVariety.find_or_create_by!(
-            plant_variety_name: lines_or_varieties[plant_id]['relation_record_name']
-          ) do |new_plant_variety|
-            new_plant_variety.assign_attributes(common_data)
-          end
-          accession_relation = { plant_variety: plant_variety }
-
-        elsif lines_or_varieties[plant_id]['relation_class_name'] == 'PlantLine'
-          plant_line = PlantLine.find_by(
-            plant_line_name: lines_or_varieties[plant_id]['relation_record_name']
-          )
-          if plant_line
-            accession_relation = { plant_line: plant_line }
-          else
-            #TODO Inform the user there is no PlantLine of a given name and that it should be submitted with another submission first
-            rollback(2)
-          end
-
-        else
-          raise "Incorrect value [#{lines_or_varieties[plant_id]['relation_class_name']}] user for relation name for a new accession."
-        end
-
-        new_plant_accession.assign_attributes(common_data.merge(accession_relation))
-      end
-
-      design_factor = nil
-      factor_values = design_factors[plant_id]
-      if design_factor_names.present? && factor_values.present?
-        factor_attrs = {}
-        factor_values.each_with_index do |factor_value, i|
-          factor_attrs["design_factor_#{i+1}".to_sym] = if design_factor_names[i]
-                                                          "#{design_factor_names[i]}_#{factor_value}"
-                                                        else
-                                                          nil
-                                                        end
-        end
-        design_factor = DesignFactor.create!(
-          common_data.except(:user, :published, :published_on).merge(
-            design_unit_counter: factor_values.last
-          ).merge(
-            factor_attrs
-          )
-        )
-      end
+      design_factor = create_design_factor(plant_id)
 
       new_plant_scoring_unit = PlantScoringUnit.create!(
         common_data.merge(scoring_unit_name: plant_id,
@@ -199,5 +208,21 @@ class Submission::PlantTrialFinalizer
 
   def describe_design_factors(design_factor_names)
     design_factor_names.compact.join(' / ')
+  end
+
+  def design_factors
+    submission.content.step04.design_factors || {}
+  end
+
+  def design_factor_names
+    submission.content.step04.design_factor_names || []
+  end
+
+  def accessions
+    submission.content.step04.accessions
+  end
+
+  def lines_or_varieties
+    submission.content.step04.lines_or_varieties || {}
   end
 end
