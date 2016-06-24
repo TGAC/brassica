f601de4adfca6856dcf64b7f64c03bb761021885
Tomasz Gubała <t.gubala@cyfronet.pl>
2016-06-24 10:29:23 +0200

    removes properties which are not paired with form fields

--- app/forms/submissions/trial/step04_content_form.rb
+++ app/forms/submissions/trial/step04_content_form.rb
@@ -1,13 +1,6 @@
 module Submissions
   module Trial
     class Step04ContentForm < PlantTrialForm
-      property :trait_mapping
-      property :trait_scores
-      property :accessions
-      property :lines_or_varieties
-      property :replicate_numbers
-      property :design_factor_names
-      property :design_factors
       property :upload_id
 
       def upload
