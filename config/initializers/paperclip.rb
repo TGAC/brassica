3de2978ee0c2b0e21342972eab8de83e5c4256dc
Tomasz Szymczyszyn <tomasz.szymczyszyn@gmail.com>
2016-08-05 10:37:55 +0200

    Allow application/vnd.ms-excel content type for uploads

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,7 @@
 Paperclip::Attachment.default_options[:url] = "/system/:class/:hash.:extension"
 Paperclip::Attachment.default_options[:hash_secret] =  Rails.application.secrets.paperclip_hash_secret
+
+Paperclip.options[:content_type_mappings] = {
+  csv: %w(application/vnd.ms-excel text/plain text/csv),
+  txt: %w(application/vnd.ms-excel text/plain text/csv)
+}
