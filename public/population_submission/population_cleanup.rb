2f492423b10ab62b7ad1e1abc1ade7079c503058
Tomasz Gubała <t.gubala@cyfronet.pl>
2016-08-11 13:01:34 +0200

    initial working version of the population cleanup script

--- public/population_submission/population_cleanup.rb
+++ public/population_submission/population_cleanup.rb
@@ -13,8 +13,7 @@ if ARGV.size < 2
   exit 1
 end
 
-# @client = Net::HTTP.new('bip.earlham.ac.uk', 443)
-@client = Net::HTTP.new('localhost', 3000)
+@client = Net::HTTP.new('bip.earlham.ac.uk', 443)
 @client.use_ssl = true
 @client.verify_mode = OpenSSL::SSL::VERIFY_NONE
 @headers = {
@@ -22,48 +21,52 @@ end
   'X-BIP-Api-Key' => ARGV[1]
 }
 
-def test_error(response, request)
+def test_error(response)
   unless response['reason'].nil?
     STDERR.puts "Encountered the following BIP request error."
     STDERR.puts response['reason']
-    if response['reason'] == 'This resource is already published and irrevocable'
-      @untouchables << request.path.split('/').last.to_i
-    else
-      STDERR.puts "Exiting."
-      exit 1
-    end
+    STDERR.puts "Exiting."
+    exit 1
   end
 end
 
 def call_bip(request)
   response = @client.request(request)
   if response.body
-    JSON.parse(response.body).tap{ |parsed| test_error(parsed, request) }
+    JSON.parse(response.body).tap{ |parsed| test_error(parsed) }
   else
     nil
   end
 end
 
-def pluralize_class(class_name)
-  class_name == 'plant_variety' ? 'plant_varieties' : "#{class_name}s"
+# Tries to delete a record
+# Reports an error if it is impossible, yet does not stop the script execution
+def delete_record(plural_name, id)
+  request = Net::HTTP::Delete.new("/api/v1/#{plural_name}/#{id}", @headers)
+  response = @client.request(request)
+  unless response['reason'].nil?
+    STDERR.puts "Encountered the following BIP request error."
+    STDERR.puts response['reason']
+  end
 end
 
+# Removes PL and related PA and PV, if they belong to the user,
+# and if they are removable.
+def delete_plant_line(plant_line_id)
+  request = Net::HTTP::Get.new("/api/v1/plant_lines?only_mine=true&plant_line[query][id]=#{plant_line_id}", @headers)
+  response = call_bip request
 
-def delete_mine(plural_name, query = '')
-  @untouchables = []
-  records_count = 0
-  puts "Removing owned #{plural_name}"
-  begin
-    request = Net::HTTP::Get.new("/api/v1/#{plural_name}?only_mine=true#{query}", @headers)
-    response = call_bip request
-    records_count = response[plural_name].count
-    if records_count > 0 && records_count > @untouchables.size
-      response[plural_name].each do |record|
-        request = Net::HTTP::Delete.new("/api/v1/#{plural_name}/#{record['id']}", @headers)
-        response = call_bip request
-      end
+  if response['plant_lines'].count > 0
+    plant_line = response['plant_lines'][0]
+    puts "Found Plant Line #{plant_line['plant_line_name']}. Removing related Plant Accessions and a Plant Variety."
+
+    plant_line['plant_accession_ids'].each do |plant_accession_id|
+      delete_record 'plant_accessions', plant_accession_id
     end
-  end while records_count > 0 && records_count > @untouchables.size
+
+    delete_record 'plant_varieties', plant_line['plant_variety_id']
+    delete_record 'plant_lines', plant_line['id']
+  end
 end
 
 
@@ -77,10 +80,14 @@ if response['plant_populations'].count == 0
   exit 1
 end
 
-plant_population_id = response['plant_populations'][0]['id']
+plant_population = response['plant_populations'][0]
+puts "Found the Plant Population, id = #{plant_population['id']}."
 
-puts "Found the Plant Population, id = #{plant_population_id}."
 
-# Step 2. Searching for owned Plant Lines related to this population
-puts "Removing owned Plant Lines related to this population."
-delete_mine('plant_lines', "&plant_line[query][plant_populations.id]=#{plant_population_id}")
+# Step 2. Traversing Plant Lines related to this population and deleting all related PVs and PAs
+
+plant_population['plant_line_ids'].each do |plant_line_id|
+  delete_plant_line(plant_line_id)
+end
+
+delete_record 'plant_populations', plant_population['id']
