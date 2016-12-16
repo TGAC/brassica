f60b179474689e48d5d86b192a51e87b16e4b4c8
Tomasz Gubała <t.gubala@cyfronet.pl>
2016-12-17 00:26:29 +0100

    squeezing 3 PT exporter csv files into one

--- app/services/submission/plant_trial_exporter.rb
+++ app/services/submission/plant_trial_exporter.rb
@@ -2,8 +2,6 @@ class Submission::PlantTrialExporter < Submission::Exporter
   def documents
     {
       plant_trial: plant_trial,
-      plant_scoring_units: plant_scoring_units,
-      plant_accessions: plant_accessions,
       trait_descriptors: trait_descriptors,
       trait_scoring: trait_scoring
     }.reject { |_,v| v.nil? }
@@ -16,26 +14,30 @@ class Submission::PlantTrialExporter < Submission::Exporter
                       { id: submitted_object.id }
   end
 
-  def plant_scoring_units
-    generate_document PlantScoringUnit,
-                      { id: submitted_object.plant_scoring_units.pluck(:id) }
-  end
-
-  def plant_accessions
-    generate_document PlantAccession,
-                      { id: submitted_object.plant_scoring_units.pluck(:plant_accession_id) }
-  end
-
   def trait_descriptors
     generate_document TraitDescriptor,
                       { id: submitted_object.trait_descriptors.map(&:id) }
   end
 
   def trait_scoring
-    data = submitted_object.scoring_table_data(@submission.user.id)
+    data = submitted_object.scoring_table_data(user_id: @submission.user.id, extended: true)
     return nil if data.empty?
     CSV.generate(headers: true) do |csv|
-      csv << ['Scoring unit name'] + submitted_object.decorate.trait_headers
+      csv << [
+        I18n.t('tables.plant_scoring_units.scoring_unit_name'),
+        I18n.t('tables.plant_accessions.plant_accession'),
+        I18n.t('tables.plant_lines.plant_line_name'),
+        I18n.t('tables.plant_varieties.plant_variety_name'),
+        I18n.t('tables.plant_accessions.plant_accession_derivation'),
+        I18n.t('tables.plant_accessions.originating_organisation'),
+        I18n.t('tables.plant_accessions.year_produced'),
+        I18n.t('tables.plant_accessions.date_harvested'),
+        I18n.t('tables.plant_scoring_units.number_units_scored'),
+        I18n.t('tables.plant_scoring_units.scoring_unit_sample_size'),
+        I18n.t('tables.plant_scoring_units.scoring_unit_frame_size'),
+        I18n.t('tables.design_factors.design_factors'),
+        I18n.t('tables.plant_scoring_units.date_planted')
+      ] + submitted_object.decorate.trait_headers
       data.each { |row| csv << row[0...-1] }  # Drop the BIP internal id
     end
   end
--- spec/services/submission/plant_trial_exporter_spec.rb
+++ spec/services/submission/plant_trial_exporter_spec.rb
@@ -9,10 +9,15 @@ RSpec.describe Submission::PlantTrialExporter do
   describe "#documents" do
     it 'produces properly formatted all trial submission CSV documents' do
       psus = [
-        create(:plant_scoring_unit, plant_trial: plant_trial,
-                                    plant_accession: create(:plant_accession, plant_line: plant_line)),
-        create(:plant_scoring_unit, plant_trial: plant_trial,
-                                    plant_accession: create(:plant_accession, :with_variety))
+        create(:plant_scoring_unit,
+               plant_trial: plant_trial,
+               plant_accession: create(:plant_accession, plant_line: plant_line)),
+        create(:plant_scoring_unit,
+               plant_trial: plant_trial,
+               plant_accession: create(:plant_accession, :with_variety)),
+        create(:plant_scoring_unit,
+               plant_trial: plant_trial,
+               plant_accession: create(:plant_accession, plant_line: create(:plant_line)))
       ]
       tds = create_list(:trait_descriptor, 2).sort_by(&:id)
       ts1 = create(:trait_score, plant_scoring_unit: psus[0], trait_descriptor: tds[0])
@@ -21,40 +26,32 @@ RSpec.describe Submission::PlantTrialExporter do
 
       documents = subject.documents
 
-      expect(documents.size).to eq 5
+      expect(documents.size).to eq 3
       expect(documents[:plant_trial].lines.size).to eq 2
       expect(documents[:plant_trial].lines[1].split(',')[0]).
         to eq plant_trial.plant_trial_name
-
-      expect(documents[:plant_scoring_units].lines.size).to eq 3
-      expect(documents[:plant_scoring_units].lines[1,2].map{ |l| l.split(',')[0] }).
-        to match_array psus.map(&:scoring_unit_name)
-
-      expect(documents[:plant_accessions].lines.size).to eq 3
-      expect(documents[:plant_accessions].lines[1,2].map{ |l| l.split(',')[0] }).
-        to match_array psus.map{ |psu| psu.plant_accession.plant_accession }
-      expect(documents[:plant_accessions].lines[1,2].map{ |l| l.split(',')[2] }).
-        to match_array [
-          psus[0].plant_accession.plant_line.plant_variety.plant_variety_name,
-          psus[1].plant_accession.plant_variety.plant_variety_name
-        ]
 
       expect(documents[:trait_descriptors].lines.size).to eq 3
       expect(documents[:trait_descriptors].lines[1,2].map{ |l| l.split(',')[1] }).
         to match_array tds.map(&:trait_name)
 
-      expect(documents[:trait_scoring].lines.size).to eq 3
-      expect(documents[:trait_scoring].lines[0].strip.split(',')).
-        to eq ['Scoring unit name', "#{tds[0].trait_name} rep1", "#{tds[0].trait_name} rep2", tds[1].trait_name]
+      expect(documents[:trait_scoring].lines.size).to eq 4
+      expect(documents[:trait_scoring].lines[0].strip.split(',')[13,3]).
+        to eq ["#{tds[0].trait_name} rep1", "#{tds[0].trait_name} rep2", tds[1].trait_name]
 
-      expect(documents[:trait_scoring].lines[1,2].map{ |l| l.split(',')[0] }).
+      generated_scores = CSV.parse(documents[:trait_scoring]).map{ |row| row[13,3] }[1,3]
+      expect(documents[:trait_scoring].lines[1,3].map{ |l| l.split(',')[0] }).
         to match_array psus.map(&:scoring_unit_name)
-      expect(documents[:trait_scoring].lines[1,2].map{ |l| l.split(',')[1] }).
-        to match_array [ts1.score_value, '-']
-      expect(documents[:trait_scoring].lines[1,2].map{ |l| l.split(',')[2] }).
-        to match_array [ts2.score_value, '-']
-      expect(documents[:trait_scoring].lines[1,2].map{ |l| l.strip.split(',')[3] }).
-        to match_array [ts3.score_value, '-']
+      expect(generated_scores.map{ |l| l[0] }).to match_array [ts1.score_value, '-', '-']
+      expect(generated_scores.map{ |l| l[1] }).to match_array [ts2.score_value, '-', '-']
+      expect(generated_scores.map{ |l| l[2] }).to match_array [ts3.score_value, '-', '-']
+
+      expect(documents[:trait_scoring].lines[1,3].map{ |l| l.split(',')[3] }).
+        to match_array [
+          psus[0].plant_accession.plant_line.plant_variety.plant_variety_name,
+          psus[1].plant_accession.plant_variety.plant_variety_name,
+          '' # A case of PA -> PL with no PV in PL
+        ]
     end
 
     it 'produces no documents for no-data cases' do
