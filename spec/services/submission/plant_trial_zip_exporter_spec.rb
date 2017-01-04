a160e8ddee3c0657880808003f4c57c8053768ec
Tomasz Gubała <t.gubala@cyfronet.pl>
2017-01-04 11:49:20 +0100

    cache testing for zip exporter

--- spec/services/submission/plant_trial_zip_exporter_spec.rb
+++ spec/services/submission/plant_trial_zip_exporter_spec.rb
@@ -7,8 +7,10 @@ RSpec.describe Submission::PlantTrialZipExporter do
   end
 
   describe "#call" do
+    before(:each) { Rails.cache.clear }
+
     it 'compress three files in the zip file' do
-      csv_data = Submission::PlantTrialZipExporter.new.call(plant_trial, 'no_cache').read
+      csv_data = Submission::PlantTrialZipExporter.new.call(plant_trial, 'cache_key').read
       file = Tempfile.new('plant_trial')
       file.write(csv_data)
       file.close
@@ -20,6 +22,18 @@ RSpec.describe Submission::PlantTrialZipExporter do
           to match_array %w(plant_trial.csv trait_descriptors.csv trait_scoring.csv)
         expect(data.map{ |d| d[1].lines.size }).to match_array [2,2,2]
       end
+    end
+
+    it 'caches response when same cache key is used' do
+      expect_any_instance_of(Submission::PlantTrialExporter).to receive(:documents).once.and_call_original
+      Submission::PlantTrialZipExporter.new.call(plant_trial, 'same_cache_key').read
+      Submission::PlantTrialZipExporter.new.call(plant_trial, 'same_cache_key').read
+    end
+
+    it 'refreshes cache when different cache key is used' do
+      expect(plant_trial).to receive(:scoring_table_data).twice.and_call_original
+      Submission::PlantTrialZipExporter.new.call(plant_trial, 'one_cache_key').read
+      Submission::PlantTrialZipExporter.new.call(plant_trial, 'another_cache_key').read
     end
   end
 end
