901fadd87645c51c478dfabc26a1596470674f3a
Tomasz Gubała <t.gubala@cyfronet.pl>
2017-01-06 19:17:14 +0100

    detecting submission of plant lines already present in BIP

--- app/services/submission/plant_line_parser.rb
+++ app/services/submission/plant_line_parser.rb
@@ -39,8 +39,8 @@ class Submission::PlantLineParser
 
   def parse_header
     header = csv.readline
-    if header.blank? || header.size < 3
-      @upload.errors.add(:file, :no_header)
+    if header.blank?
+      @upload.errors.add(:file, :no_header_plant_lines)
     elsif header.index('Plant line').nil?
       @upload.errors.add(:file, :no_plant_line_header)
     elsif header.index('Plant variety').nil?
@@ -105,13 +105,16 @@ class Submission::PlantLineParser
       @upload.log "Ignored row for #{plant_line_name} since a plant line with that name is already defined in the uploaded file."
     # elsif @current_plant_lines.include? plant_line_name
     #   @upload.log "Ignored row for #{plant_line_name} since a plant line with that name is already defined. Please clear the 'Plant line list' field before re-uploading a CSV file."
+    elsif reused_plant_line?(plant_line_name)
+      @upload.log "Ignored row for #{plant_line_name} since a plant line with that name is already present in BIP."\
+                  "Please use the 'Plant line list' field to add this existing plant line to the submitted population."
     elsif species.blank?
       @upload.log "Ignored row for #{plant_line_name} since Species name is missing."
     elsif existing_plant_accession?(plant_accession, originating_organisation)
       @upload.log "Ignored row for #{plant_line_name} since it refers to a plant accession which currently exists in BIP."
     elsif incomplete_plant_accession?(plant_accession, originating_organisation, year_produced)
-      @upload.log "Ignored row for #{plant_line_name} since incomplete plant accession was given.
-                       Either all or none of the Plant accession, Originating organisation and Year produced values must be provided."
+      @upload.log "Ignored row for #{plant_line_name} since incomplete plant accession was given."\
+                  "Either all or none of the Plant accession, Originating organisation and Year produced values must be provided."
     elsif reused_plant_accession?(plant_accession, originating_organisation, year_produced)
       @upload.log "Ignored row for #{plant_line_name} since the defined plant accession was already used for another plant line in this file."
     elsif TaxonomyTerm.find_by_name(species).blank?
@@ -143,6 +146,10 @@ class Submission::PlantLineParser
 
   def incomplete_plant_accession?(*pa_data)
     pa_data.any?(&:present?) && pa_data.any?(&:blank?)
+  end
+
+  def reused_plant_line?(plant_line_name)
+    PlantLine.find_by(plant_line_name: plant_line_name)
   end
 
   def reused_plant_accession?(plant_accession, originating_organisation, year_produced)
