4d8f517f5148445340ccb60277090265779d9b1e
Tomasz Szymczyszyn <tomasz.szymczyszyn@gmail.com>
2017-04-03 00:24:41 +0200

    Fixed template generators - no spaces in trait names

--- app/services/analysis/gwas/genotype_csv_template_generator.rb
+++ app/services/analysis/gwas/genotype_csv_template_generator.rb
@@ -3,10 +3,10 @@ class Analysis
     class GenotypeCsvTemplateGenerator
       def call
         CSV.generate do |csv|
-          csv << ["ID",                 "SNP 1",  "SNP 2",  "SNP 3",  "SNP 4"]
-          csv << ["Plant sample id 1",  1,        "NA",      1,       2]
-          csv << ["Plant sample id 2",  0,        1,         1,       0]
-          csv << ["Plant sample id 3",  "NA",     0,         1,       2]
+          csv << ["ID",                 "SNP-1",  "SNP-2",  "SNP-3",  "SNP-4"]
+          csv << ["Plant-sample-id-1",  1,        "NA",      1,       2]
+          csv << ["Plant-sample-id-2",  0,        1,         1,       0]
+          csv << ["Plant-sample-id-3",  "NA",     0,         1,       2]
         end
       end
     end
--- app/services/analysis/gwas/phenotype_csv_template_generator.rb
+++ app/services/analysis/gwas/phenotype_csv_template_generator.rb
@@ -3,10 +3,10 @@ class Analysis
     class PhenotypeCsvTemplateGenerator
       def call
         CSV.generate do |csv|
-          csv << ["ID",                 "Trait 1",  "Trait 2",  "Trait 3", "Trait 4"]
-          csv << ["Plant sample id 1",  0.125,      "A",        5,         "yes"]
-          csv << ["Plant sample id 2",  0.025,      "B",        10,        "no"]
-          csv << ["Plant sample id 3",  0.0,        "C",        7,         "no"]
+          csv << ["ID",                 "Trait-1",  "Trait-2",  "Trait-3", "Trait-4"]
+          csv << ["Plant-sample-id-1",  0.125,      "A",        5,         "yes"]
+          csv << ["Plant-sample-id-2",  0.025,      "B",        10,        "no"]
+          csv << ["Plant-sample-id-3",  0.0,        "C",        7,         "no"]
         end
       end
     end
