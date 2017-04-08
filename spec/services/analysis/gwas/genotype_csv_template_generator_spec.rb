2303f9f2670d06cce475bfe95132d2325d77a960
Tomasz Szymczyszyn <tomasz.szymczyszyn@gmail.com>
2017-04-08 18:25:51 +0200

    Fix outdated specs for CSV template generators

--- spec/services/analysis/gwas/genotype_csv_template_generator_spec.rb
+++ spec/services/analysis/gwas/genotype_csv_template_generator_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Analysis::Gwas::GenotypeCsvTemplateGenerator do
   subject { described_class.new.call }
 
   it "returns a CSV string" do
-    expect(subject.split("\n").first).to eq("ID,SNP 1,SNP 2,SNP 3,SNP 4")
+    expect(subject.split("\n").first).to eq("ID,SNP-1,SNP-2,SNP-3,SNP-4")
   end
 end
