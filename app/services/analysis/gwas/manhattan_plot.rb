ff198419a6209c8a6859eeaad7d463e34e53a4c4
Tomasz Szymczyszyn <tomasz.szymczyszyn@gmail.com>
2017-05-31 17:34:53 +0200

    ManhattanPlot refactoring

--- app/services/analysis/gwas/manhattan_plot.rb
+++ app/services/analysis/gwas/manhattan_plot.rb
@@ -7,52 +7,59 @@ class Analysis
         @analysis = analysis
       end
 
+      def process_trait(data_file)
+        neg_log10_p_values = {}
+        mutations = nil
+        tooltips = []
+
+        # TODO: looks brittle - maybe metadata should be added to datafiles?
+        trait = data_file.file_file_name.match(/SNPAssociation-Full-(.*)\.csv$/)[1]
+
+        CSV.foreach(data_file.file.path) do |name, _, neg_log10_p|
+          # TODO: fix GWASSER so that it does not output invalid row ID.1
+          next if name =~ /\AID(\.1)?\z/
+
+          # GWASSER replaces dashes in mutation names with dots...
+          name = name.gsub(".", "-")
+
+          neg_log10_p_values[name] = neg_log10_p
+        end
+
+        if map_csv_data_file = analysis.data_files.gwas_map.first
+          File.open(map_csv_data_file.file.path, "r") do |file|
+            map_data = Analysis::Gwas::MapCsvParser.new.call(file)
+            mutations = map_data.csv.map do |name, chrom, pos|
+              [name, neg_log10_p_values[name], chrom.to_i, pos.to_i]
+            end
+          end
+
+          mutations = mutations.sort do |mut_a, mut_b|
+            chrom_a, pos_a = mut_a[-2..-1]
+            chrom_b, pos_b = mut_b[-2..-1]
+
+            chrom_a == chrom_b ? pos_a <=> pos_b : chrom_a <=> chrom_b
+          end
+
+        else
+          mutations = neg_log10_p_values.to_a
+        end
+
+        tooltips = mutations.map { |mut| format_tooltip(*mut) }
+
+        [trait, mutations, tooltips]
+      end
+
       def call
         { traits: [], chromosomes: [] }.tap do |results|
 
           chromosomes = Hash.new { |h, k| h[k] = [] }
 
-          result_data_files.each.with_index do |data_file, trait_idx|
-            neg_log10_p_values = {}
-            mutations = nil
-            tooltips = []
+          result_data_files.each do |data_file|
+            trait, mutations, tooltips = process_trait(data_file)
 
-            # TODO: looks brittle - maybe metadata should be added to datafiles?
-            trait = data_file.file_file_name.match(/SNPAssociation-Full-(.*)\.csv$/)[1]
-
-            CSV.foreach(data_file.file.path) do |name, _, neg_log10_p|
-              # TODO: fix GWASSER so that it does not output invalid row ID.1
-              next if name =~ /\AID(\.1)?\z/
-
-              # GWASSER replaces dashes in mutation names with dots...
-              name = name.gsub(".", "-")
-
-              neg_log10_p_values[name] = neg_log10_p
-            end
-
-            if map_csv_data_file = analysis.data_files.gwas_map.first
-              File.open(map_csv_data_file.file.path, "r") do |file|
-                map_data = Analysis::Gwas::MapCsvParser.new.call(file)
-                mutations = map_data.csv.map do |name, chrom, pos|
-                  [name, neg_log10_p_values[name], chrom.to_i, pos.to_i]
-                end
-              end
-
-              mutations = mutations.sort do |mut_a, mut_b|
-                chrom_a, pos_a = mut_a[-2..-1]
-                chrom_b, pos_b = mut_b[-2..-1]
-
-                chrom_a == chrom_b ? pos_a <=> pos_b : chrom_a <=> chrom_b
-              end
-
-              mutations.each_with_index do |(_, _, chrom, _), idx|
-                chromosomes[chrom] << idx
-              end
-            else
-              mutations = neg_log10_p_values.to_a
-            end
-
-            tooltips = mutations.map { |mut| format_tooltip(*mut) }
+            mutations.each_with_index do |(_, _, chrom, _), idx|
+              chromosomes[chrom] << idx
+            end if mutations.first.size > 2
 
             results[:traits] << [trait, mutations, tooltips]
           end
