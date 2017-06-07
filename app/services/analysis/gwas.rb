bd69d278184ad28dfa8487ef8681217e0fe99b85
Tomasz Szymczyszyn <tomasz.szymczyszyn@gmail.com>
2017-06-07 16:16:56 +0200

    Normalize input CSV and skip irrelevant SNPs

--- app/services/analysis/gwas.rb
+++ app/services/analysis/gwas.rb
@@ -17,9 +17,22 @@ class Analysis
     attr_reader :analysis
 
     def prepare_csv_data_files
-      return if genotype_data_file(:csv)
+      geno_csv_data_file = genotype_data_file(:csv)
 
-      vcf_data_file = genotype_data_file
+      if !geno_csv_data_file
+        # NOTE: no need to normalize as coversion already outputs correct files
+        convert_genotype_vcf_to_csv
+      elsif geno_csv_data_file.uploaded?
+        mutations_to_remove = find_mutations_to_remove(geno_csv_data_file)
+        normalize_csv(geno_csv_data_file, remove_columns: mutations_to_remove)
+        normalize_csv(map_data_file, remove_rows: mutations_to_remove) if map_data_file
+      end
+
+      normalize_csv(phenotype_data_file)
+    end
+
+    def convert_genotype_vcf_to_csv
+      vcf_data_file = genotype_data_file(:vcf)
 
       genotype_csv, map_csv = Analysis::Gwas::GenotypeVcfToCsvConverter.new.call(vcf_data_file.file.path)
 
@@ -40,6 +53,55 @@ class Analysis
         file_content_type: "text/csv",
         owner: analysis.owner
       )
+    end
+
+    def find_mutations_to_remove(geno_csv_data_file)
+      values_by_mutation = Hash.new { |h, k| h[k] = Set.new }
+
+      CSV.open(geno_csv_data_file.file.path) do |csv|
+        headers = csv.readline
+
+        csv.each do |row|
+          row.each.with_index do |val, col_idx|
+            values_by_mutation[headers[col_idx]] << val
+          end
+        end
+      end
+
+      values_by_mutation.map { |mutation, values| mutation if (values - ["NA"]).size < 2 }.compact - ["ID"]
+    end
+
+    def normalize_csv(data_file, remove_columns: [], remove_rows: [])
+      normalized_csv = CSV.generate(force_quotes: true) do |csv|
+        CSV.open(data_file.file.path, "r") do |existing_csv|
+          headers = existing_csv.readline
+          remove_col_idxes = remove_columns.map { |col| headers.index(col) }
+
+          existing_csv.rewind
+          existing_csv.each.with_index do |row, row_idx|
+            next if remove_rows.include?(row[0])
+
+            csv << row.map.with_index do |val, col_idx|
+              next if remove_col_idxes.include?(col_idx)
+
+              if row_idx == 0 || col_idx == 0
+                val.strip.gsub(/\W/, '_')
+              else
+                val.strip
+              end
+            end.compact
+          end
+        end
+      end
+
+      # TODO: store normalized files as separate ones with origin == "generated"
+      Tempfile.open([File.basename(data_file.file.path, ".csv") + "-normalized", ".csv"]) do |file|
+        file << normalized_csv
+        file.flush
+        file.rewind
+
+        data_file.update!(file: file)
+      end
     end
 
     def runner
@@ -87,12 +149,18 @@ class Analysis
 
     def genotype_data_file(type = nil)
       scope = analysis.data_files.gwas_genotype
-      scope = scope.csv if type
-      scope.first
+      scope = scope.send(type) if type
+      scope.generated.first || scope.uploaded.first
     end
 
     def phenotype_data_file
-      analysis.data_files.gwas_phenotype.first
+      scope = analysis.data_files.gwas_phenotype
+      scope.generated.first || scope.uploaded.first
+    end
+
+    def map_data_file
+      scope = analysis.data_files.gwas_map
+      scope.generated.first || scope.uploaded.first
     end
 
     def gwas_script
