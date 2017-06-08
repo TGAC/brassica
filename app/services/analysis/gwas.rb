b471df867ed0d3ca0415a7b43031b020cc0c51a5
Tomasz Szymczyszyn <tomasz.szymczyszyn@gmail.com>
2017-06-08 19:08:58 +0200

    Extract CSV data file preparation to a separate class

--- app/services/analysis/gwas.rb
+++ app/services/analysis/gwas.rb
@@ -6,7 +6,7 @@ class Analysis
     end
 
     def call
-      prepare_csv_data_files
+      setup.call
       runner.call(job_command) do
         store_results
       end
@@ -16,113 +16,8 @@ class Analysis
 
     attr_reader :analysis
 
-    def prepare_csv_data_files
-      geno_csv_data_file = genotype_data_file(:csv)
-
-      if !geno_csv_data_file
-        # NOTE: no need to normalize as coversion already outputs correct files
-        convert_genotype_vcf_to_csv
-      elsif geno_csv_data_file.uploaded?
-        mutations_to_remove = find_mutations_to_remove(geno_csv_data_file)
-        normalize_csv(geno_csv_data_file, remove_columns: mutations_to_remove)
-        normalize_csv(map_data_file, remove_rows: mutations_to_remove) if map_data_file
-      end
-
-      if plant_trial_based?
-      prepare_plant_trial_phenotype_csv_data_file
-      else
-      normalize_csv(phenotype_data_file)
-      end
-    end
-
-    def convert_genotype_vcf_to_csv
-      vcf_data_file = genotype_data_file(:vcf)
-
-      genotype_csv, map_csv = Analysis::Gwas::GenotypeVcfToCsvConverter.new.call(vcf_data_file.file.path)
-
-      analysis.data_files.create!(
-        role: :input,
-        origin: :generated,
-        data_type: :gwas_genotype,
-        file: genotype_csv,
-        file_content_type: "text/csv",
-        owner: analysis.owner
-      )
-
-      analysis.data_files.create!(
-        role: :input,
-        origin: :generated,
-        data_type: :gwas_map,
-        file: map_csv,
-        file_content_type: "text/csv",
-        owner: analysis.owner
-      )
-    end
-
-    def prepare_plant_trial_phenotype_csv_data_file
-      plant_trial = PlantTrial.visible(analysis.owner_id).find(analysis.meta.fetch("plant_trial_id"))
-      phenotype_csv = Analysis::Gwas::PlantTrialPhenotypeCsvBuilder.new.build_csv(plant_trial)
-
-      analysis.data_files.create!(
-        role: :input,
-        data_type: :gwas_phenotype,
-        file: phenotype_csv,
-        file_content_type: "text/csv",
-        owner: analysis.owner
-      )
-    end
-
-    def plant_trial_based?
-      analysis.meta["plant_trial_id"].present?
-    end
-
-    def find_mutations_to_remove(geno_csv_data_file)
-      values_by_mutation = Hash.new { |h, k| h[k] = Set.new }
-
-      CSV.open(geno_csv_data_file.file.path) do |csv|
-        headers = csv.readline
-
-        csv.each do |row|
-          row.each.with_index do |val, col_idx|
-            values_by_mutation[headers[col_idx]] << val
-          end
-        end
-      end
-
-      values_by_mutation.map { |mutation, values| mutation if (values - ["NA"]).size < 2 }.compact - ["ID"]
-    end
-
-    def normalize_csv(data_file, remove_columns: [], remove_rows: [])
-      normalized_csv = CSV.generate(force_quotes: true) do |csv|
-        CSV.open(data_file.file.path, "r") do |existing_csv|
-          headers = existing_csv.readline
-          remove_col_idxes = remove_columns.map { |col| headers.index(col) }
-
-          existing_csv.rewind
-          existing_csv.each.with_index do |row, row_idx|
-            next if remove_rows.include?(row[0])
-
-            csv << row.map.with_index do |val, col_idx|
-              next if remove_col_idxes.include?(col_idx)
-
-              if row_idx == 0 || col_idx == 0
-                val.strip.gsub(/\W/, '_')
-              else
-                val.strip
-              end
-            end.compact
-          end
-        end
-      end
-
-      # TODO: store normalized files as separate ones with origin == "generated"
-      Tempfile.open([File.basename(data_file.file.path, ".csv") + "-normalized", ".csv"]) do |file|
-        file << normalized_csv
-        file.flush
-        file.rewind
-
-        data_file.update!(file: file)
-      end
+    def setup
+      @setup ||= Analysis::Gwas::Setup.new(analysis)
     end
 
     def runner
@@ -154,7 +49,7 @@ class Analysis
 
     def job_command
       args = [
-        "--gFile", genotype_data_file(:csv).file.path,
+        "--gFile", genotype_data_file.file.path,
         "--pFile", phenotype_data_file.file.path,
         "--outDir", runner.results_dir,
         "--noPlots"
@@ -168,9 +63,8 @@ class Analysis
       ([gwas_script] + args).join(" ")
     end
 
-    def genotype_data_file(type = nil)
-      scope = analysis.data_files.gwas_genotype
-      scope = scope.send(type) if type
+    def genotype_data_file
+      scope = analysis.data_files.gwas_genotype.csv
       scope.generated.first || scope.uploaded.first
     end
 
---
+++ app/services/analysis/gwas/setup.rb
@@ -0,0 +1,129 @@
+class Analysis
+  class Gwas
+    class Setup
+      def initialize(analysis)
+        @analysis = analysis
+      end
+
+      # TODO: make nicer names for generated / normalized data files
+      def call
+        geno_csv_data_file = genotype_data_file(:csv)
+
+        if !geno_csv_data_file
+          # NOTE: no need to normalize as conversion already outputs correct files
+          convert_genotype_vcf_to_csv
+        elsif geno_csv_data_file.uploaded?
+          mutations_to_remove = find_mutations_to_remove(geno_csv_data_file)
+          normalize_csv(geno_csv_data_file, remove_columns: mutations_to_remove)
+          normalize_csv(map_data_file, remove_rows: mutations_to_remove) if map_data_file
+        end
+
+        if plant_trial_based?
+          prepare_plant_trial_phenotype_csv_data_file
+        else
+          normalize_csv(phenotype_data_file)
+        end
+      end
+
+      private
+
+      attr_reader :analysis
+
+      def convert_genotype_vcf_to_csv
+        vcf_data_file = genotype_data_file(:vcf)
+
+        genotype_csv, map_csv = Analysis::Gwas::GenotypeVcfToCsvConverter.new.call(vcf_data_file.file.path)
+
+        create_csv_data_file(genotype_csv, data_type: :gwas_genotype)
+        create_csv_data_file(map_csv, data_type: :gwas_map)
+      end
+
+      def prepare_plant_trial_phenotype_csv_data_file
+        plant_trial = PlantTrial.visible(analysis.owner_id).find(analysis.meta.fetch("plant_trial_id"))
+        phenotype_csv = Analysis::Gwas::PlantTrialPhenotypeCsvBuilder.new.build_csv(plant_trial)
+
+        create_csv_data_file(phenotype_csv, data_type: :gwas_phenotype)
+      end
+
+      def plant_trial_based?
+        analysis.meta["plant_trial_id"].present?
+      end
+
+      def find_mutations_to_remove(geno_csv_data_file)
+        values_by_mutation = Hash.new { |h, k| h[k] = Set.new }
+
+        CSV.open(geno_csv_data_file.file.path) do |csv|
+          headers = csv.readline
+
+          csv.each do |row|
+            row.each.with_index do |val, col_idx|
+              values_by_mutation[headers[col_idx]] << val
+            end
+          end
+        end
+
+        values_by_mutation.map { |mutation, values| mutation if (values - ["NA"]).size < 2 }.compact - ["ID"]
+      end
+
+      def normalize_csv(data_file, remove_columns: [], remove_rows: [])
+        normalized_csv = CSV.generate(force_quotes: true) do |csv|
+          CSV.open(data_file.file.path, "r") do |existing_csv|
+            headers = existing_csv.readline
+            remove_col_idxes = remove_columns.map { |col| headers.index(col) }
+
+            existing_csv.rewind
+            existing_csv.each.with_index do |row, row_idx|
+              next if remove_rows.include?(row[0])
+
+              csv << row.map.with_index do |val, col_idx|
+                next if remove_col_idxes.include?(col_idx)
+
+                if row_idx == 0 || col_idx == 0
+                  val.strip.gsub(/\W/, '_')
+                else
+                  val.strip
+                end
+              end.compact
+            end
+          end
+        end
+
+        # TODO: store normalized files as separate ones with origin == "generated"
+        Tempfile.open([File.basename(data_file.file.path, ".csv") + "-normalized", ".csv"]) do |file|
+          file << normalized_csv
+          file.flush
+          file.rewind
+
+          data_file.update!(file: file)
+        end
+      end
+
+      def genotype_data_file(type = nil)
+        scope = analysis.data_files.gwas_genotype
+        scope = scope.send(type) if type
+        scope.generated.first || scope.uploaded.first
+      end
+
+      def phenotype_data_file
+        scope = analysis.data_files.gwas_phenotype
+        scope.generated.first || scope.uploaded.first
+      end
+
+      def map_data_file
+        scope = analysis.data_files.gwas_map
+        scope.generated.first || scope.uploaded.first
+      end
+
+      def create_csv_data_file(file, data_type:)
+        analysis.data_files.create!(
+          role: :input,
+          origin: :generated,
+          data_type: data_type,
+          file: file,
+          file_content_type: "text/csv",
+          owner: analysis.owner
+        )
+      end
+    end
+  end
+end
