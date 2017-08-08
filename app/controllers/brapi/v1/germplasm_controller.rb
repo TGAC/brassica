afca158775099b4a0db06729a812004d1aab3888
Carlos Horro <carlos.horro@gmail.com>
2017-08-08 16:44:01 +0100

    germplasm-search count_mode fix

--- app/controllers/brapi/v1/germplasm_controller.rb
+++ app/controllers/brapi/v1/germplasm_controller.rb
@@ -26,7 +26,7 @@ class Brapi::V1::GermplasmController < Brapi::BaseController
         page_size: page_size
       }
           
-      result_object = germplasm_queries.germplasm_search_query(query_params, false)
+      result_object = germplasm_queries.germplasm_search_query(query_params, count_mode: false)
       
       records = result_object.values
      
@@ -47,7 +47,7 @@ class Brapi::V1::GermplasmController < Brapi::BaseController
         
         # pagination data returned
         
-        result_count_object = germplasm_queries.germplasm_search_query(query_params, true)
+        result_count_object = germplasm_queries.germplasm_search_query(query_params, count_mode: true)
         total_count = result_count_object.values.first[0].to_i
         total_pages = (total_count/page_size.to_f).ceil
         
