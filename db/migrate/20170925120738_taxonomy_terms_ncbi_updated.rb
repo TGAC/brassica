2b877e84ac6ba8fa51aaf167b8e5c2af9372d059
Carlos Horro <carlos.horro@gmail.com>
2017-09-25 15:30:26 +0200

    Added migration for NCBI taxon ids to taxonomy_terms

---
+++ db/migrate/20170925120738_taxonomy_terms_ncbi_updated.rb
@@ -0,0 +1,23 @@
+require 'csv'   
+
+class TaxonomyTermsNcbiUpdated < ActiveRecord::Migration
+  def up
+    add_column :taxonomy_terms, :ncbi_taxon, :string 
+    
+    TaxonomyTerm.reset_column_information
+ 
+    filename = "db/taxonomy_terms_ncbi_updated.csv"
+    
+    CSV.foreach(filename, :headers => true) do |row|
+      sql = <<-SQL
+        UPDATE  taxonomy_terms set ncbi_taxon = '#{row["ncbi_taxon"]}' WHERE taxonomy_terms.id = #{row["ID"]}
+      SQL
+      connection.execute(sql)
+    end
+   
+  end
+  
+  def down
+    remove_column :taxonomy_terms, :ncbi_taxon
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202160421) do
+ActiveRecord::Schema.define(version: 20170925120738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170202160421) do
   end
 
   add_index "api_keys", ["token"], name: "api_keys_token_idx", unique: true, using: :btree
+  add_index "api_keys", ["token"], name: "index_api_keys_on_token", using: :btree
 
   create_table "countries", force: :cascade do |t|
     t.string   "country_code", limit: 3, null: false
@@ -279,6 +280,7 @@ ActiveRecord::Schema.define(version: 20170202160421) do
   end
 
   add_index "plant_accessions", ["plant_accession", "originating_organisation", "year_produced"], name: "plant_accessions_pa_oo_yp_idx", unique: true, using: :btree
+  add_index "plant_accessions", ["plant_accession"], name: "plant_accessions_plant_accession_idx", using: :btree
   add_index "plant_accessions", ["plant_line_id"], name: "plant_accessions_plant_line_id_idx", using: :btree
   add_index "plant_accessions", ["plant_variety_id"], name: "index_plant_accessions_on_plant_variety_id", using: :btree
   add_index "plant_accessions", ["published"], name: "index_plant_accessions_on_published", using: :btree
@@ -730,9 +732,11 @@ ActiveRecord::Schema.define(version: 20170202160421) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.boolean  "canonical",        default: true
+    t.string   "ncbi_taxon"
   end
 
   add_index "taxonomy_terms", ["label"], name: "index_taxonomy_terms_on_label", using: :btree
+  add_index "taxonomy_terms", ["name"], name: "index_taxonomy_terms_on_name", unique: true, using: :btree
   add_index "taxonomy_terms", ["name"], name: "taxonomy_terms_name_idx", unique: true, using: :btree
   add_index "taxonomy_terms", ["taxonomy_term_id"], name: "index_taxonomy_terms_on_taxonomy_term_id", using: :btree
 
@@ -832,6 +836,7 @@ ActiveRecord::Schema.define(version: 20170202160421) do
   end
 
   add_index "traits", ["label"], name: "index_traits_on_label", using: :btree
+  add_index "traits", ["name"], name: "index_traits_on_name", unique: true, using: :btree
   add_index "traits", ["name"], name: "traits_name_idx", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
