4e9a549f5d20136f0a663539f7ace916c90d7844
Tomasz Szymczyszyn <tomasz.szymczyszyn@gmail.com>
2018-01-15 13:29:52 +0100

    Add GAPIT analysis type

--- spec/models/analysis_spec.rb
+++ spec/models/analysis_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Analysis do
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_most(250) }
   it { should validate_numericality_of(:associated_pid).only_integer.allow_nil }
-  it { should define_enum_for(:analysis_type).with(%w(gwasser)) }
+  it { should define_enum_for(:analysis_type).with(%w(gwasser gapit)) }
   it { should define_enum_for(:status).with(%w(idle running success failure)) }
   it { should have_many(:data_files).class_name("Analysis::DataFile") }
   it { should have_db_column(:finished_at).of_type(:datetime) }
