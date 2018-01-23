487591a61d8ec89e5b10260adf8e535cb374f09f
Tomasz Szymczyszyn <tomasz.szymczyszyn@gmail.com>
2018-01-23 12:56:31 +0100

    -> config

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,9 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
-require 'csv'
 # require "rails/test_unit/railtie"
+
+require 'csv'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -26,10 +27,8 @@ module Brassica
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
 
     config.autoload_paths += [
       "#{config.root}/app/forms",
